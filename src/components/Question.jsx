65de2a1bf06e38419ade9be544e8ba462a166d7a
LuisGordilloTadeo2003 <luisgordi@outlook.com>
2025-02-19 18:33:20 +0000

    creacion de carpetas y estructura de la aplicacion

---
+++ src/components/Question.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+const Question = () => {
+    const [question, setQuestion] = useState();
+
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default Question;
